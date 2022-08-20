80ebd371f4caa37a1792607b321f42776e51a05a
simon89user <simonbekishev@hotmail.fr>
2022-08-20 15:27:03 +0200

    feat sass

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["upload.wikimedia.org", "flagcdn.com"],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
