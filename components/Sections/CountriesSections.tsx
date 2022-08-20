2a84d26a5cf1fe84b4e32a7e8e5f12defa64170a
simon89user <simonbekishev@hotmail.fr>
2022-08-20 15:30:28 +0200

    feat country section for homepage

---
+++ components/Sections/CountriesSections.tsx
@@ -0,0 +1,28 @@
+import { Country } from "../../types/Country";
+import CountryCard from "../Cards/CountryCard";
+
+const CountriesSection = ({ countries }: { countries: Country[] | null }) => {
+  let content;
+  if (!countries) {
+    content = (
+      <div>
+        <p>Sorry, no country to display</p>
+      </div>
+    );
+  } else {
+    content = (
+      <div className="container-fluid">
+        <div className="row">
+          {countries.map((country) => (
+            <div className="col-12 col-lg-3">
+              <CountryCard country={country} />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  return <div>{content}</div>;
+};
+
+export default CountriesSection;
