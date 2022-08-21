aced5839937cfb1ee652b8765cf718d11ddff1fe
simon89user <simonbekishev@hotmail.fr>
2022-08-21 11:50:16 +0200

    ui desktop country page

--- pages/countries/[code].tsx
+++ pages/countries/[code].tsx
@@ -13,92 +13,132 @@ const Country = ({
   country: Country;
   borderCountries: { name: string; code: string }[];
 }) => {
-
   return (
     <>
       <BackNavigation />
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-6">
-            <div className="image-wrapper">
-              <Image src={country.flags?.png ?? logo} height={500} width={500} alt="" />
+      <div className="container">
+        <div className={styles.countryContainer}>
+          <div className="row">
+            <div className="col-lg-6">
+              <div className={styles.imageContainer}>
+                <div className={styles.imageWrapper}>
+                  <Image
+                    src={country.flags?.png ?? logo}
+                    layout="fill"
+                    className={styles.image}
+                    alt={`${country} offical flag`}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="col-lg-6">
-            <div className={styles.informationWrapper}>
-              <div className={styles.titleWrapper}>
-                <h1 className={styles.title}>{country.name}</h1>
-              </div>
-              <div className="row">
-                <div className="col-lg-6">
-                  <div className={styles.detailsWrapper}>
-                    <div className={styles.detail}>
-                      <p>
-                        <span className={styles.detailTitle}>
-                          Native Name:{" "}
-                        </span>
-                        {country.nativeName ?? "unknown"}
-                      </p>
+            <div className="col-lg-6">
+              <div
+                className={
+                  styles.informationContainer +
+                  " d-flex h-100 flex-column justify-content-center"
+                }
+              >
+                <div className={styles.titleWrapper}>
+                  <h1 className={styles.title}>{country.name}</h1>
+                </div>
+                <div className="row">
+                  <div className="col-lg-6">
+                    <div className={styles.detailsWrapper}>
+                      <div className={styles.detail}>
+                        <p>
+                          <span className={styles.detailTitle}>
+                            Native Name:{" "}
+                          </span>
+                          {country.nativeName ?? "unknown"}
+                        </p>
+                      </div>
+                      <div className={styles.detail}>
+                        <p>
+                          <span className={styles.detailTitle}>
+                            Population:{" "}
+                          </span>
+                          {country.population ?? "unknown"}
+                        </p>
+                      </div>
+                      <div className={styles.detail}>
+                        <p>
+                          <span className={styles.detailTitle}>Region: </span>
+                          {country.region ?? "unknown"}
+                        </p>
+                      </div>
+                      <div className={styles.detail}>
+                        <p>
+                          <span className={styles.detailTitle}>
+                            Sub region:{" "}
+                          </span>
+                          {country.subregion ?? "unknown"}
+                        </p>
+                      </div>
+                      <div className={styles.detail}>
+                        <p>
+                          <span className={styles.detailTitle}>Capital: </span>
+                          {country.capital ?? "unknown"}
+                        </p>
+                      </div>
                     </div>
-                    <div className={styles.detail}>
-                      <p>
-                        <span className={styles.detailTitle}>Population: </span>
-                        {country.population ?? "unknown"}
-                      </p>
-                    </div>
-                    <div className={styles.detail}>
-                      <p>
-                        <span className={styles.detailTitle}>Region: </span>
-                        {country.region ?? "unknown"}
-                      </p>
-                    </div>
-                    <div className={styles.detail}>
-                      <p>
-                        <span className={styles.detailTitle}>Sub region: </span>
-                        {country.subregion ?? "unknown"}
-                      </p>
-                    </div>
-                    <div className={styles.detail}>
-                      <p>
-                        <span className={styles.detailTitle}>Capital: </span>
-                        {country.capital ?? "unknown"}
-                      </p>
+                  </div>
+                  <div className="col-lg-6">
+                    <div className={styles.detailsWrapper}>
+                      <div className={styles.detail}>
+                        <p>
+                          <span className={styles.detailTitle}>
+                            Top Level Domain:
+                          </span>{" "}
+                          {country.topLevelDomain ?? "unknown"}
+                        </p>
+                      </div>
+                      <div className={styles.detail}>
+                        <p>
+                          <span className={styles.detailTitle}>
+                            Currencies:
+                          </span>{" "}
+                          {country.currencies
+                            ?.map((currency) => currency.name)
+                            .join(", ")}
+                        </p>
+                      </div>
+                      <div className={styles.detail}>
+                        <p>
+                          <span className={styles.detailTitle}>Languages: </span>
+                          {country.languages
+                            ?.map((language) => language.name)
+                            .join(", ")}
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="col-lg-6">
-                  <div className={styles.detailsWrapper}>
-                    <div className={styles.detail}>
-                      <p>
-                        <span className={styles.detailTitle}>
-                          Top Level Domain:
-                        </span>
-                        {country.topLevelDomain ?? "unknown"}
-                      </p>
-                    </div>
-                    <div className={styles.detail}>
-                      <p>
-                        <span className={styles.detailTitle}>Currencies: </span>
-                        {country.currencies?.map((currency) => currency.name)}
-                      </p>
-                    </div>
-                    <div className={styles.detail}>
-                      <p>
-                        <span className={styles.detailTitle}>Languages: </span>
-                        {country.languages.map(
-                          (language) => language.name + " "
+                <div className="col-12">
+                  <div className={styles.borderWrapper}>
+                    <div className="d-flex flex-row">
+                      <div>
+                        <p style={{ fontWeight: 600, whiteSpace: "nowrap" }}>
+                          Border Countries:{" "}
+                        </p>
+                      </div>
+                      <div style={{ marginLeft: "1rem" }}>
+                        {borderCountries.map((border) => (
+                          <Link
+                            key={border.code}
+                            href={`/countries/${border.code}`}
+                          >
+                            <a className={styles.btnBorder + " btn btn-light"}>
+                              {border.name}
+                            </a>
+                          </Link>
+                        ))}
+                        {borderCountries.length === 0 && (
+                          <p>None, this is an island</p>
                         )}
-                      </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div>
-                {borderCountries.map((border) => (
-                  <Link key={border.code} href={`/countries/${border.code}`}>
-                    <a className="btn btn-outline-primary">{border.name}</a>
-                  </Link>
-                ))}
               </div>
             </div>
           </div>
